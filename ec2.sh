9027869cfb468d0380cfb380719614d433718f49
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-06-21 14:07:43 +0900

    feat: githubのkeyでsshできるように追加

--- ec2.sh
+++ ec2.sh
@@ -6,6 +6,9 @@ GITHUB_USERNAME="moriyoshi-kasuga"
 curl https://github.com/${GITHUB_USERNAME}.keys -o ${GITHUB_USERNAME}.keys
 
 mkdir -p /home/ubuntu/.ssh/
-mv ${GITHUB_USERNAME}.keys /home/ubuntu/.ssh/authorized_keys
+chmod 700 /home/ubuntu/.ssh/
+chown ubuntu:ubuntu /home/ubuntu/.ssh/
 
+mv ${GITHUB_USERNAME}.keys /home/ubuntu/.ssh/authorized_keys
 chmod 600 /home/ubuntu/.ssh/authorized_keys
+chown ubuntu:ubuntu /home/ubuntu/.ssh/authorized_keys
--- ec2.tf
+++ ec2.tf
@@ -24,7 +24,37 @@ resource "aws_instance" "kdg-aws-20250621-user-date-enable" {
     UserDate = "true"
   }
 
+  security_groups             = [aws_security_group.ssh_enable.name]
   user_data_replace_on_change = true
 
   user_data = file("./ec2.sh")
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC where the EC2 instance will be launched"
+  type        = string
+}
+
+data "aws_vpc" "main" {
+  id = var.vpc_id
+}
+
+resource "aws_security_group" "ssh_enable" {
+  name        = "ssh-enable"
+  description = "Allow SSH access from anywhere"
+  vpc_id      = data.aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
