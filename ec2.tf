20b35ff956b55a4c06287bab970ad6714f479d7e
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-06-21 12:03:57 +0900

    feat: add ec2

---
+++ ec2.tf
@@ -0,0 +1,30 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "kdg-aws-20250621-user-date-enable" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name     = "kdg-aws-20250621-user-date-enable",
+    UserDate = "true"
+  }
+
+  user_data_replace_on_change = true
+
+  user_data = file("./ec2.sh")
+}
